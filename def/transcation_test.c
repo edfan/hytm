db84d39bf7abc062b936d3b694876645294fb187
endrias <endrias@mit.edu>
2017-12-16 17:42:44 -0800

    def version compiles

--- def/transcation_test.c
+++ def/transcation_test.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 #include <sys/param.h>
 #include <pthread.h>
+#include <assert.h>
+#include "software_tran.h"
 static volatile int counter;
-void * increment_counter(int i ){
-    printf("size of unsigned %d ", sizeof(unsigned int ));
-    int INCRMENT_COUNT = 500;
-    for(int i = 0; i < INCRMENT_COUNT; i++ ){
+int INCREMENT_COUNT = 500;
+void * increment_counter(){
+
+    for(int i = 0; i < INCREMENT_COUNT; i++ ){
         our_xbegin();
-        for(int i = 0; i < INCRMENT_COUNT; i++){
+        for(int j = 0; j < INCREMENT_COUNT; j++){
             counter++;
         }
         our_xend();
     }
-
+    return NULL;
 }
-int main() {
-    pthread_t threads [8];
 
-    for(int i = 0; i < 8; i++){
+int main() {
+    int NUM_THREADS = 8;
+    pthread_t threads [NUM_THREADS];
+
+    for(int i = 0; i < NUM_THREADS; i++){
         pthread_create(&threads[i], NULL, increment_counter,
-                       (void*)i);
+                       NULL);
     }
-    for(int i = 0; i < 8; i++){
+    for(int i = 0; i < NUM_THREADS; i++){
        pthread_join(threads[i], NULL);
     }
-
+    assert(counter == INCREMENT_COUNT*INCREMENT_COUNT*NUM_THREADS);
     printf("counter value %d \n", counter);
     return 0;
 }
